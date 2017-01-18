2c24efe13a9b16ac0595d276589dce298d70e4b1
Mathieu THEBAUD <math.thebaud@gmail.com>
2017-01-18 15:20:07 +0100

    -Fix import (create index and add type) -Search request

--- elasticsearch/README.md
+++ elasticsearch/README.md
@@ -19,10 +19,91 @@ GET <nom de votre index>/_count
 
 ## Requêtes
 
-À vous de jouer ! Écrivez les requêtes ElasticSearch permettant de résoudre les problèmes posés.
+### Compter le nombre d'appels autour de Lansdale dans un rayon de 500 mètres
 
 ```
-TODO : ajouter les requêtes ElasticSearch ici
+GET calls/call/_search
+{
+    "query": {
+        "bool" : {
+            "must" : {
+                "match_all" : {}
+            },
+            "filter" : {
+                "geo_distance" : {
+                    "distance" : "500m",
+                    "location" : {
+                        "lat" :  40.241493,
+                        "lon" : -75.283783
+                    }
+                }
+            }
+        }
+    }
+}
+```
+
+### Compter le nombre d'appels par catégorie
+
+```
+GET calls/call/_search
+{
+  "size": 0,
+  "aggs": {
+    "group_by_state": {
+      "terms": {
+        "field": "cat"
+      }
+    }
+  }
+}
+```
+
+### Trouver les 3 mois ayant comptabilisés le plus d'appels
+
+```
+GET calls/call/_search
+{
+    "aggs" : {
+        "callOnMonth" : {
+            "date_histogram" : {
+                "field" : "date",
+                "interval" : "month",
+                 "order":{
+                     "_count":"desc"
+                 }
+            }
+        }
+    }
+}
+```
+
+### Trouver le top 3 des villes avec le plus d'appels pour overdose
+
+```
+GET calls/call/_search
+{
+   "aggs":{
+      "findOverdose":{
+         "filter":{
+            "term":{
+               "desc":"overdose"
+            }
+         },
+         "aggs":{
+            "callsByCity":{
+               "terms":{
+                  "field":"city",
+                  "size":3,
+                  "order":{
+                     "_count":"desc"
+                  }
+               }
+            }
+         }
+      }
+   }
+}
 ```
 
 ## Kibana
--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,28 +7,62 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
-var calls = [];
-fs.createReadStream('../911.csv')
-    .pipe(csv())
-    .on('data', data => {
-		var call = {
-			location : {
-				lat : parseFloat(data.lat),
-				lng : parseFloat(data.lng)
-			},
-			cat : data.title.split(':')[0],
-			desc : data.title.split(':')[1].trim(),
-			date : new Date(Date.parse(data.timeStamp+' GMT')),
-			city : data.twp
-		};
-		calls.push({ index: { _index: 'calls', _type: 'call'}});
-		calls.push(call);
-    })
-    .on('end', () => {
-		esClient.bulk({
-			body: calls
-		},function(err,resp){
-			console.log('Data indexed');
-			console.log('Error '+err);
-		});
-    });
+//Create index and mapping
+esClient.indices.create({
+	index : 'calls'
+},function(err,resp){
+	if(!!err){
+		console.log(err);
+		return;
+	}
+	console.log('Index created');
+	var body = {
+		call:{
+			properties:{
+				location : { "type" : "geo_point" },
+				cat : { "type" : "string", fielddata : true },
+				desc : {"type" : "string", fielddata : true},
+				date : {"type" : "date"},
+				city : {"type" : "keyword"}
+			}
+		}
+	};
+	esClient.indices.putMapping({index:"calls", type:"call", body:body},function(err,resp){
+		console.log('Mapping created');
+		if(!!err){
+			console.log(err);
+			return;
+		}
+		//Import
+		var calls = [];
+		fs.createReadStream('../911.csv')
+			.pipe(csv())
+			.on('data', data => {
+				var call = {
+					location : {
+						lat : parseFloat(data.lat),
+						lon : parseFloat(data.lng)
+					},	
+					cat : data.title.split(':')[0],
+					desc : data.title.split(':')[1].trim(),
+					date : new Date(Date.parse(data.timeStamp+' GMT')),
+					city : data.twp
+				};
+				calls.push({ index: { _index: 'calls', _type: 'call'}});
+				calls.push(call);
+			})
+			.on('end', () => {
+				esClient.bulk({
+					body: calls
+				},function(err,resp){
+					console.log('Data indexed');
+					if(!!err){
+						console.log('Error : '+err);				
+					}
+				});
+			});
+	});
+});
+
+
+
